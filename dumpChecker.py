2fd804f9d92421f2648355481410b24710d1894e
Jonni <cohenj14@gmail.com>
2021-04-05 12:54:05 -0700

    Update dumpChecker.py
    Passing encoding to read txt files.

--- dumpChecker.py
+++ dumpChecker.py
@@ -2,7 +2,7 @@ import glob  # used to grab dump files
 import credentials  # pass in personal information from different file
 
 for file in glob.glob('*.txt'):  # group dump files together
-    with open(file) as f:  # read from files
+    with open(file, encoding="utf8") as f:  # read from files
         for line in f:  # read by line
             if credentials.my_number in line:  # find if personal information in dump
                 print(line)
