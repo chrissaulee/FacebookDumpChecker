bf5bba2ac521d5446076c53cab0267cf5d19ad2e
chris <csl7dk@virginia.edu>
2021-04-05 15:30:23 -0400

    Beginning Workings of Checking Facebook Data Dump

---
+++ dumpChecker.py
@@ -0,0 +1,8 @@
+import glob # used to grab dump files
+import credentials # pass in personal information from different file
+
+for file in glob.glob('*.txt'): # group dump files together
+    with open(file) as f: # read from files
+        for line in f: # read by line
+            if credentials.my_number in line: # find if personal information in dump
+                print(line)
